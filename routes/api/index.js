10312690bf3d339b9f8da0578ebe48ba652df257
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2018-04-16 20:44:32 -0400

    router adjust for heroku

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,13 @@ const router = require("express").Router();
 const eventRoutes = require("./events");
 const rosterRoutes = require("./roster");
 
+//api routes
 router.use("/roster", rosterRoutes);
 router.use("/events", eventRoutes);
+
+//if no routes hit call react app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
