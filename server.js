e77ba0a7e8d8c55fe047200416f79fe9a33e6f0a
Aaron Cross <aaroncross@aarons-mbp.lan>
2018-04-17 20:33:22 -0400

    Fixed it

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
