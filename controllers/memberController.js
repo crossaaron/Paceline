6df4c2f25baa88b2f286132b5f150bdad193cf7b
Aaron Cross <aaroncross@aarons-mbp.lan>
2018-04-17 20:25:36 -0400

    .

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    console.log(req.query);
+    res.json("got here")
     db.Member.find(req.query)
       .then(dbMembers => {res.json(dbMembers)})
       .catch(err => res.json(err).status(422));
