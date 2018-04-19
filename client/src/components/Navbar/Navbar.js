b06ca7d6b3ec3bdc4b73ba3f9697582a17bae2ca
zrcarter2017 <zcarter@allegiancy.us>
2018-04-19 16:57:01 -0400

    fixed navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,53 +3,75 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 // Navbar component
 const Navbar = props => (
-
-
-      <div className="navbar navbar-fixed-top ">
-        <div className="container justify-content-center">
+  <nav>
+    <div className="navbar navbar-fixed-top" role="navigation">
+      <div className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
           <div className="navbar-header">
-
-            <Link className="navbar-brand" to="/"
-
-            // className={
-            //         window.location.pathname === "/"
-            //           ? "nav-link active"
-            //           : "nav-link"
-            //       }
+            {/* <button
+              type="button"
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#collapse"
             >
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button> */}
             <Link id="navbar-brand" className="navbar-brand" to="/home">
-              Home
+              HOME
             </Link>
-            <Link className="navbar-brand" to="/roster"
-
-            // className={
-            //         window.location.pathname === "/"
-            //           ? "nav-link active"
-            //           : "nav-link"
-            //       }
-            >
-              Roster
+            <Link id="navbar-brand" className="navbar-brand" to="/roster">
+              ROSTER
             </Link>
-            <Link className="navbar-brand" to="/events"
-
-            // className={
-            //         window.location.pathname === "/"
-            //           ? "nav-link active"
-            //           : "nav-link"
-            //       }
-
-                  >
-              Events
+            <Link id="navbar-brand" className="navbar-brand" to="/events">
+              EVENTS
             </Link>
+          </div>
+          <div className="navbar-collapse collapse" id="collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <Link
+                  to="/"
+                  className={
+                    window.location.pathname === "/"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/roster"
+                  className={
+                    window.location.pathname === "/roster"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Roster
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/events"
+                  className={
+                    window.location.pathname === "/events"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  Events
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-
-
+    </div>
+  </nav>
 );
 
 export default Navbar;
